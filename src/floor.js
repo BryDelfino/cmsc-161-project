df96b6ab4782c25ce7ee4cc945a8c13b164c5577
BryDelfino <bvdelfino@up.edu.ph>
2026-05-16 23:47:38 +0800

    feat: added a uv scale variable for setting texture size per unit.

--- src/floor.js
+++ src/floor.js
@@ -8,11 +8,12 @@ class Floor extends TexturedMeshNode {
       -s, -2, -s, 1,  s, -2,  s, 1, -s, -2,  s, 1,
     ]), gl.STATIC_DRAW);
 
+    const uvScale = s / 5; // Repeats every 10 units (much larger clumps)
     const uvBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-      0, 0,  s/2, 0,  s/2, s/2,
-      0, 0,  s/2, s/2,  0, s/2,
+      0, 0,  uvScale, 0,  uvScale, uvScale,
+      0, 0,  uvScale, uvScale,  0, uvScale,
     ]), gl.STATIC_DRAW);
 
     super({
