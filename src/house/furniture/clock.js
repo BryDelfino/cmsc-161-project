31c1745377c69fcd57a7f13bf8be7d7a7fcefd4b
BryDelfino <bvdelfino@up.edu.ph>
2026-05-22 12:03:55 +0800

    feat: Made grandfather clock hollow. Created a visible pendelum which swings.

--- src/house/furniture/clock.js
+++ src/house/furniture/clock.js
@@ -2,11 +2,12 @@ class GrandfatherClock extends Node {
   constructor(gl, solidRes) {
     super();
     this.parts = [];
+    this.time = 0;
 
     const brassColor = [0.75, 0.65, 0.25, 1.0];
     const clockFaceColor = [0.95, 0.95, 0.9, 1.0];
     const handsColor = [0.1, 0.1, 0.1, 1.0];
-    const woodColor = [0.28, 0.18, 0.11, 1.0];
+    const woodColor = [0.65, 0.45, 0.28, 1.0];
 
     // 1. Base (bottom section) - Y = 0 to 0.5, height = 0.5, center at 0.25
     const base = new Wall(gl, solidRes.program, solidRes.locs, woodColor);
@@ -16,28 +17,72 @@ class GrandfatherClock extends Node {
     this.base = base;
 
     // 2. Waist (middle section) - Y = 0.5 to 1.7, height = 1.2, center at 1.1
-    const waist = new Wall(gl, solidRes.program, solidRes.locs, woodColor);
-    waist.setParent(this);
-    waist.translate([0, 1.1, 0]);
-    waist.scale([0.5, 1.2, 0.35]);
-    this.waist = waist;
+    // We make it hollow by building it out of thin panels: back, left, right, and front borders (left, right, top, bottom)
+    const waistBack = new Wall(gl, solidRes.program, solidRes.locs, woodColor);
+    waistBack.setParent(this);
+    waistBack.translate([0, 1.1, -0.165]);
+    waistBack.scale([0.5, 1.2, 0.02]);
+    this.waistBack = waistBack;
 
-    // Waist glass window cutout (overlayed black/blue glass pane, semi-transparent)
-    const windowGlass = new Wall(gl, solidRes.program, solidRes.locs, [0.1, 0.15, 0.2, 0.5]); // semi-transparent blue glass
+    const waistLeft = new Wall(gl, solidRes.program, solidRes.locs, woodColor);
+    waistLeft.setParent(this);
+    waistLeft.translate([-0.24, 1.1, 0]);
+    waistLeft.scale([0.02, 1.2, 0.35]);
+    this.waistLeft = waistLeft;
+
+    const waistRight = new Wall(gl, solidRes.program, solidRes.locs, woodColor);
+    waistRight.setParent(this);
+    waistRight.translate([0.24, 1.1, 0]);
+    waistRight.scale([0.02, 1.2, 0.35]);
+    this.waistRight = waistRight;
+
+    const frontLeft = new Wall(gl, solidRes.program, solidRes.locs, woodColor);
+    frontLeft.setParent(this);
+    frontLeft.translate([-0.21, 1.1, 0.165]);
+    frontLeft.scale([0.08, 1.2, 0.02]);
+    this.frontLeft = frontLeft;
+
+    const frontRight = new Wall(gl, solidRes.program, solidRes.locs, woodColor);
+    frontRight.setParent(this);
+    frontRight.translate([0.21, 1.1, 0.165]);
+    frontRight.scale([0.08, 1.2, 0.02]);
+    this.frontRight = frontRight;
+
+    const frontTop = new Wall(gl, solidRes.program, solidRes.locs, woodColor);
+    frontTop.setParent(this);
+    frontTop.translate([0, 1.625, 0.165]);
+    frontTop.scale([0.5, 0.15, 0.02]);
+    this.frontTop = frontTop;
+
+    const frontBottom = new Wall(gl, solidRes.program, solidRes.locs, woodColor);
+    frontBottom.setParent(this);
+    frontBottom.translate([0, 0.575, 0.165]);
+    frontBottom.scale([0.5, 0.15, 0.02]);
+    this.frontBottom = frontBottom;
+
+    // Waist glass window cutout (overlayed glass pane, semi-transparent blue glass)
+    const windowGlass = new Wall(gl, solidRes.program, solidRes.locs, [0.1, 0.15, 0.2, 0.5]);
     windowGlass.setParent(this);
     windowGlass.translate([0, 1.1, 0.18]);
     windowGlass.scale([0.34, 0.9, 0.01]);
     this.windowGlass = windowGlass;
 
+    // Pendulum Swing Parent Node (acts as pivot at Y = 1.5)
+    const pendulumNode = new Node();
+    pendulumNode.setParent(this);
+    this.pendulumNode = pendulumNode;
+
     // Pendulum (inside the waist behind the glass)
+    // Shaft: local height = 0.8, centered at local Y = -0.4
     const pendulumShaft = new Cylinder(gl, solidRes.program, solidRes.locs, 0.01, 0.01, 0.8, 8, brassColor);
-    pendulumShaft.setParent(this);
-    pendulumShaft.translate([0, 1.1, 0.05]);
+    pendulumShaft.setParent(pendulumNode);
+    pendulumShaft.translate([0, -0.4, 0]);
     this.parts.push(pendulumShaft);
 
+    // Bob: local disc at the bottom of the shaft (local Y = -0.8)
     const pendulumBob = new Cylinder(gl, solidRes.program, solidRes.locs, 0.1, 0.1, 0.02, 16, brassColor);
-    pendulumBob.setParent(this);
-    pendulumBob.translate([0, 0.75, 0.06]);
+    pendulumBob.setParent(pendulumNode);
+    pendulumBob.translate([0, -0.8, 0.01]);
     pendulumBob.rotate(Math.PI / 2, [1, 0, 0]); // face forward
     this.parts.push(pendulumBob);
 
@@ -136,12 +181,29 @@ class GrandfatherClock extends Node {
     return { minX, maxX, minY, maxY, minZ, maxZ };
   }
 
+  update(deltaTime) {
+    this.time += deltaTime;
+    // Swing back and forth with speed 3.0 rad/s and max amplitude of 0.12 radians
+    const angle = 0.12 * Math.sin(this.time * 3.0);
+    if (this.pendulumNode) {
+      this.pendulumNode.localMatrix = mat4.create();
+      mat4.translate(this.pendulumNode.localMatrix, this.pendulumNode.localMatrix, [0, 1.5, 0.05]);
+      mat4.rotate(this.pendulumNode.localMatrix, this.pendulumNode.localMatrix, angle, [0, 0, 1]);
+    }
+  }
+
   draw(gl, viewProjection) {
     this.updateWorldMatrix(this.parent ? this.parent.worldMatrix : null);
 
     // Draw wood solid segments
     this.base.draw(gl, viewProjection);
-    this.waist.draw(gl, viewProjection);
+    this.waistBack.draw(gl, viewProjection);
+    this.waistLeft.draw(gl, viewProjection);
+    this.waistRight.draw(gl, viewProjection);
+    this.frontLeft.draw(gl, viewProjection);
+    this.frontRight.draw(gl, viewProjection);
+    this.frontTop.draw(gl, viewProjection);
+    this.frontBottom.draw(gl, viewProjection);
     this.hood.draw(gl, viewProjection);
     this.crown.draw(gl, viewProjection);
 
